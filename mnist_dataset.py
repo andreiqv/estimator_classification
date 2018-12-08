8caaa3ac2a6f0ea44008171378757519004c4dbe
z <phxv@mail.ru>
2018-12-09 02:28:40 +0300

    1

--- mnist_dataset.py
+++ mnist_dataset.py
@@ -63,9 +63,9 @@ def download(directory, filename):
 	"""Download (and unzip) a file from the MNIST dataset if not already done."""
 	filepath = os.path.join(directory, filename)
 	if tf.gfile.Exists(filepath):
-	return filepath
+		return filepath
 	if not tf.gfile.Exists(directory):
-	tf.gfile.MakeDirs(directory)
+		tf.gfile.MakeDirs(directory)
 	# CVDF mirror of http://yann.lecun.com/exdb/mnist/
 	url = 'https://storage.googleapis.com/cvdf-datasets/mnist/' + filename + '.gz'
 	_, zipped_filepath = tempfile.mkstemp(suffix='.gz')
@@ -73,7 +73,7 @@ def download(directory, filename):
 	urllib.request.urlretrieve(url, zipped_filepath)
 	with gzip.open(zipped_filepath, 'rb') as f_in, \
 		tf.gfile.Open(filepath, 'wb') as f_out:
-	shutil.copyfileobj(f_in, f_out)
+			shutil.copyfileobj(f_in, f_out)
 	os.remove(zipped_filepath)
 	return filepath
 
